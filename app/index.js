6cd1735c83b2db713267bf1b376cf1ba7c3bea5a
Eric Price <wunderkammer123@gmail.com>
2018-11-13 10:51:05 -0500

    create index.html + .js in App directory.  Create first react component

---
+++ app/index.js
@@ -0,0 +1,19 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+require('index.css');
+
+class App extends React.Component {
+
+    render(){
+        return (
+            <div>
+                Hello World!
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    docutment.getElementById('app')
+)
