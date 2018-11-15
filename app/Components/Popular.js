e6214d3c4ee37f0969503d74eb7be25d09aa8db9
Eric Price <wunderkammer123@gmail.com>
2018-11-15 15:06:35 -0500

    Build out Popular component; modify CSS

--- app/Components/Popular.js
+++ app/Components/Popular.js
@@ -1,9 +1,38 @@
 var React = require('react');
 
 class Popular extends React.Component {
+    constructor(props) {
+        super();
+        this.state = {
+          selectedLanguage: 'All',
+        };
+         this.updateLanguage = this.updateLanguage.bind(this);
+      }
+
+      updateLanguage(lang) {
+        this.setState(function () {
+          return {
+            selectedLanguage: lang,
+          }
+        });
+      }
     render() {
+        var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
+        
         return (
-            <div>Popular! Test</div>
+            <ul className='languages'>
+            {/* <p>Selected Language: {this.state.selectedLanguage}</p> */}
+                {languages.map(function (lang) {
+                    return (
+                        <li 
+                            style={lang === this.state.selectedLanguage ? { color: '#d0021b'} : null}
+                            onClick={this.updateLanguage.bind(null, lang)}
+                            key={lang}>
+                            {lang}
+                        </li> 
+                        )
+                }, this)}
+            </ul>
         )
     }
 }
